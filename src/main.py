ead7fad3e67c9d2c06e9563f079bee5a87a6ef43
Futrime <futrime@outlook.com>
2023-04-15 17:11:42 +0800

    feat: optimize example code

--- src/main.py
+++ src/main.py
@@ -6,9 +6,20 @@ import novelcraft.sdk as sdk
 async def main():
     await sdk.initialize()
     sdk.get_logger().info('Hello, world!')
-    await asyncio.sleep(30)
-    await sdk.finalize()
-    sdk.get_logger().info('Goodbye, world!')
+
+    while True:
+        # Required to keep the SDK alive
+        await asyncio.sleep(0)
+
+        agent = sdk.get_agent()
+
+        if agent is None:
+            continue
+
+        agent.set_movement(sdk.IAgent.MovementKind.FORWARD)
+
+    await ncsdk.finalize()
+    ncsdk.get_logger().info('Goodbye, world!')
 
 if __name__ == '__main__':
     asyncio.run(main())
